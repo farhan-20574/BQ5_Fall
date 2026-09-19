406d62e0c8a7dad97838e97c7e263e8e5143cb65
Farhan <farhan20574@outlook.com>
2026-09-19 12:44:49 +0000

    class end

--- Flutter/03Class/loop.dart
+++ Flutter/03Class/loop.dart
@@ -1,9 +1,18 @@
 void main(){
-list <int> even = [2,4,6,8,10];
+List <int> even = [2,4,6,8,10];
 
+int count = 0;
 even.forEach((e){
-  print(e);
+  //print(e);
+  if (e>=4){
+    // print(e);
+    count++;
+  }
+  // else{
+  //   print("not found");
+  // }
 });
+  print (count);
 
 
 }
